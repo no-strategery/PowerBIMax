7b8c2fb6dc1e6a8766d896319348282b7e1348c6
no-strategery <36918774+no-strategery@users.noreply.github.com>
2018-03-14 23:44:55 -0500

    signed onStartup test

--- scripts/background.js
+++ scripts/background.js
@@ -1,5 +1,12 @@
+function handleStartup() {
+  browser.tabs.create({
+    url:"http://chilloutandwatchsomecatgifs.com/"
+  });
+}
+
 function handleClick() {
   browser.runtime.openOptionsPage();
 }
 
+browser.runtime.onStartup.addListener(handleStartup);
 browser.browserAction.onClicked.addListener(handleClick);
