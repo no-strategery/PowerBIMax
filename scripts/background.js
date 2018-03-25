f484935929041ff0625ab44924934b2c77f6f63e
no-strategery <36918774+no-strategery@users.noreply.github.com>
2018-03-25 00:47:18 -0500

    Cleanup background.js, shortcut key for options.html added, min-version to 48 for "commands" key

--- scripts/background.js
+++ scripts/background.js
@@ -1,30 +1,4 @@
+browser.browserAction.onClicked.addListener(handleClick);
 function handleClick() {
   browser.runtime.openOptionsPage();
 }
-
-browser.browserAction.onClicked.addListener(handleClick);
-
-function tabCreate(tab) {
-  console.log('Tab id is: ' + tab.id);
-}
-
-browser.tabs.onCreated.addListener((tab)=>tabCreate(tab));
-
-function logTabsForWindows(windowInfoArray) {
-  for (windowInfo of windowInfoArray) {
-    console.log(`Window: ${windowInfo.id}`);
-    console.log(windowInfo.tabs.map((tab) => {return tab.url}));
-  }
-}
-
-function onError(error) {
-  console.log(`Error: ${error}`);
-}
-
-browser.windows.onCreated.addListener((tab) => {
-  var getting = browser.windows.getAll({
-    populate: true,
-    windowTypes: ["normal"]
-  });
-  getting.then(logTabsForWindows, onError);
-});
