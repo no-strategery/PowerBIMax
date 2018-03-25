619c34d1707b53dd91bef5d52a61286363dc9c9f
no-strategery <36918774+no-strategery@users.noreply.github.com>
2018-03-25 16:09:32 -0500

    progress

--- scripts/background.js
+++ scripts/background.js
@@ -14,25 +14,29 @@ function updateUI() {
 }
 
 function kioskPrep(data){
-  // get open windows so we can close them after making our kiosk
-  let closing = browser.windows.getAll({});
-  // open kiosk window for Site1
-  let kioskPage = browser.windows.create(
-    {
-      url:data.Site1.url,
-      type:'panel',
-      state:'fullscreen'
-    });
-  kioskPage.then({},onError);
-  // close all windows opened before the kiosk window
-  closing.then((wList)=>{
-    for (w of wList) {
-      let closing = browser.windows.remove(w.id);
-      closing.then({},onError);
-    }
-  },onError);
+  // do nothing if no kiosk pages enabled, no point
+  if ((data.Site1.enabled==true) || (data.Site2.enabled==true)
+   || (data.Site3.enabled==true) || (data.Site4.enabled==true)
+   || (data.Site5.enabled==true)) {
+    // get open windows so we can close them after making our kiosk
+    let closing = browser.windows.getAll({});
+    // open kiosk window for Site1
+    let kioskPage = browser.windows.create(
+      {
+        url:data.Site1.url,
+        type:'panel',
+        state:'fullscreen'
+      });
+    kioskPage.then({},onError);
+    // close all windows opened before the kiosk window
+    closing.then((wList)=>{
+      for (w of wList) {
+        let closing = browser.windows.remove(w.id);
+        closing.then({},onError);
+      }
+    },onError);
+  };
 }
-
 // catch-all for unkept Promises
 function onError(e) {
   console.error(e);
