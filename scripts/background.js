766a2a1954ad97182e5a2c50af8c32986ac03e03
no-strategery <36918774+no-strategery@users.noreply.github.com>
2018-03-25 03:31:53 -0500

    progress

--- scripts/background.js
+++ scripts/background.js
@@ -1,4 +1,39 @@
+// What to do when the button on toolbar is clicked
 browser.browserAction.onClicked.addListener(handleClick);
 function handleClick() {
   browser.runtime.openOptionsPage();
+}
+
+// Code to run once DOM is loaded
+document.addEventListener('DOMContentLoaded',updateUI);
+function updateUI() {
+  console.log('Webpage Rotation Kiosk loaded');
+  // Load stored data for the extension
+  let getting = browser.storage.local.get();
+  getting.then(kioskPrep, onError);
+}
+
+function kioskPrep(data){
+  // get open windows so we can close them after making our kiosk
+  let closing = browser.windows.getAll({});
+  // open kiosk window for Site1
+  let kioskPage = browser.windows.create(
+    {
+      url:data.Site1.url,
+      type:'panel',
+      state:'fullscreen'
+    });
+  kioskPage.then({},onError);
+  // close all windows opened before the kiosk window
+  closing.then((wList)=>{
+    for (w of wList) {
+      let closing = browser.windows.remove(w.id);
+      closing.then({},onError);
+    }
+  },onError);
+}
+
+// catch-all for unkept Promises
+function onError(e) {
+  console.error(e);
 }
